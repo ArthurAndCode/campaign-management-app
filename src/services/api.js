eed7dff2fdcdc03e744f0a4749bfdddca95d1786
Arthur <arturb13@protonmail.com>
2025-04-15 00:09:29 +0200

    mock dns

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-const api = axios.create({ baseURL: "http://localhost:8080/api/v1" });
+const api = axios.create({ baseURL: "https://campaign-management-production.up.railway.app/api/v1" });
 export default api;
